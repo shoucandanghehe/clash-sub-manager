818dbd0014a3cd9ebd2cf60eec0523e3c0d8ef79
shoucandanghehe <wallfjjd@gmail.com>
2026-03-16 14:57:04 +0800

    🧹 清理 WebUI 生成的 JS 文件

--- src/clash_sub_manager/webui/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createPinia } from 'pinia';
-import { createApp } from 'vue';
-import App from './App.vue';
-import vuetify from './plugins/vuetify';
-import './style.css';
-const app = createApp(App);
-app.use(createPinia());
-app.use(vuetify);
-app.mount('#app');
